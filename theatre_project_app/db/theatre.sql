cf85e227532ddc61e58da17a5e5e56aba2c1304f
Stephen Ramsay <rabrandom@gmail.com>
2019-12-15 11:54:46 +0000

    some sql was added

--- theatre_project_app/db/theatre.sql
+++ theatre_project_app/db/theatre.sql
@@ -1,2 +1,21 @@
+DROP TABLE shows;
 DROP TABLE customers;
-DROP TABLE theatre;
+
+
+CREATE TABLE shows
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  showdate VARCHAR(255) NOT NULL,
+  showtime VARCHAR(255) NOT NULL,
+  premiumDate VARCHAR(255) NOT NULL,
+  premiumTime VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE customers
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  premium BOOLEAN,
+  show INT REFERENCES shows(id)
+);
