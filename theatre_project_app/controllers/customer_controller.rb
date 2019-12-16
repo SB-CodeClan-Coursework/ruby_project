3c721c76e49a6b4ac720d82bb007b061181b7428
Stephen Ramsay <rabrandom@gmail.com>
2019-12-16 15:56:14 +0000

    feeding Alex's Git-stalking app

--- theatre_project_app/controllers/customer_controller.rb
+++ theatre_project_app/controllers/customer_controller.rb
@@ -3,9 +3,9 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/customer.rb' )
 also_reload( '../models/*' )
 
-get '/?' do
-  @customer
-end
+# get '/?' do
+#   @customer
+# end
 
 # get '/customer' do
 #   @customer = Customer.all()
